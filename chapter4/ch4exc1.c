399950ceef978f69a56baae279f0b0b10ff35c3c
orcapraveenkumar <spk427@gmail.com>
2014-09-06 12:08:07 -0700

    chapter4

---
+++ chapter4/ch4exc1.c
@@ -0,0 +1,17 @@
+/*Chapter 4
+  PRAVEENKUMAR
+  Exercise 1*/
+  
+  #include <stdio.h>
+  
+  int main (void)
+  {
+  	
+  	int F = 82;
+  	int c;
+  	
+  	c = (F - 32) / 1.8;
+  	printf ("The conversion of %i fahrenheit to degree celsius is = %i", F, c);
+  	
+	return 0;
+  }
---
+++ chapter4/ch4exc2.c
@@ -0,0 +1,17 @@
+/*Chapter 4
+  PRAVEENKUMAR
+  Exercise 5*/
+  
+  #include <stdio.h>
+  
+  int main (void)
+  {
+  	
+  	char c, d;
+  	  	
+  	c = 'd';
+  	d = c;
+  	printf ("d = %c\n", d);
+  	
+	return 0;
+  }
