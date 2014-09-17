81a4c78e41cb6c72b3ac85ebf6af78fdb242eec7
orcapraveenkumar <spk427@gmail.com>
2014-09-16 23:57:46 -0700

    chapter10

---
+++ chapter10/ch10ex3.c
@@ -0,0 +1,29 @@
+/*Chapter 10
+  PRAVEENKUMAR
+  Program 10.3 */
+  
+#include <stdio.h>
+
+int main (void)
+{
+	void concat (char result[], const char str1[], const char str2[]);
+	const char s1[] = { "Test " };
+	const char s2[] = { "works." };
+	char s3[20];
+	
+	concat ( s3, s1, s2);
+	
+	printf ("%s\n", s3);
+	
+	return 0;
+	
+}
+
+void concat (char result[], const char str1[], const char str2[])
+{
+	int i, j;
+	
+	for ( i = 0; str1[i]! ='\0'; ++i )
+		result[i]= str1[i];
+		
+}
