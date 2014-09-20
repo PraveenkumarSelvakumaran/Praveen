3c835274a56fbc5a5c71c427618e9018180bbc0e
orcapraveenkumar <spk427@gmail.com>
2014-09-20 09:34:12 -0700

    chapter10

--- chapter10/ch10ex3.c
+++ chapter10/ch10ex3.c
@@ -19,11 +19,17 @@ int main (void)
 	
 }
 
-void concat (char result[], const char str1[], const char str2[])
+void concat ( char result[], const char str1[], const char str2[] )
 {
 	int i, j;
 	
-	for ( i = 0; str1[i]! ='\0'; ++i )
-		result[i]= str1[i];
+	for ( i = 0; str1[i] != '\0'; ++i )
+		result[1] = str1[i];
 		
+	for ( j = 0; str2[j] != '\0'; ++j )
+		result[ i + j ] = str2[j];
+		
+	result [i + j] = '\0' ;
 }
+
+
