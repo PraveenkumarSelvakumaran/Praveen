2fc0fb9dda388ead573daa25ae1b9a79b2996862
orcapraveenkumar <spk427@gmail.com>
2014-09-29 06:39:52 -0700

    chapter11

---
+++ chapter11/ch11ex1.c
@@ -0,0 +1,19 @@
+/* Chapter 11
+   PRAVEENKUMAR
+   Program 11.1 */
+
+#include <stdio.h>
+
+int main (void)
+{
+	int count = 10, x;
+	int *int_pointer;
+	
+	int_pointer = &count;
+	x = *int_pointer;
+	
+	printf ("count = %i, x = %i\n", count, x);
+	
+	return 0;
+	
+}
